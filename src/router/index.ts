3e93d543aecbb8255b598d53764c98fc37279e17
gausszhou <gausszhou@qq.com>
2023-03-15 19:49:39 +0800

    docs: update README.md

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
-import Vue from 'vue'
-import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue';
+import type { RouteConfig } from 'vue-router';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
 
 Vue.use(VueRouter)
 
@@ -13,9 +14,6 @@ const routes: Array<RouteConfig> = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
 ]
